af7abc95711a663d80efac11b9597d8b528fb0ed
Emily Pepke <elpepke41@gmail.com>
2016-11-06 22:45:22 -0500

    updated child and parent classes

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,26 @@
-<?php 
+<?php
 	// this file will extend PArentClass.php
+  // the child class is for holidays
 
 	class ChildClass extends ParentClass {
-		
+    private $nameOfHoliday;
+
+    public function __construct($month, $day, $nameOfHoliday) {
+      parent::__construct($month, $day);
+      $this->$nameOfHoliday = $nameOfHoliday;
+    }
+
+    public function getName() {
+      return $this->$nameOfHoliday;
+    }
+
+    public function setName($nameOfHoliday) {
+      $this->$nameOfHoliday = $nameOfHoliday;
+    }
+
+    public function __toString() {
+      $parent = parent::__toString();
+      $child = "Wowee! Today is also " . $this->getName();
+      return $parent . $child;
+    }
 	}
--- ParentClass.php
+++ ParentClass.php
@@ -1,6 +1,33 @@
 <?php
 	// This is the file for the parent class
+  //parent class will be a day of the year
 
 	class ParentClass {
-		
+    private $month;
+    private $day;
+
+    public function __construct($month, $day) {
+      $this->$month = $month;
+      $this->$day = $day;
+    }
+
+    public function getMonth() {
+      return $this->$month;
+    }
+
+    public function getDay() {
+      return $this->$day;
+    }
+
+    public function setMonth($month) {
+      $this->$month = $month;
+    }
+
+    public function setDate($day) {
+      $this->$day = $day;
+    }
+
+    public function __toString() {
+      return "Today's date is " . getMonth() . " " . getDay();
+    }
 	}
